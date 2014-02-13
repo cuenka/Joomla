414e594b922e901b7c005501f12f49ebcd2bdbc0
cuenka <cuenka@gmail.com>
2014-02-13 16:04:17 +0000

    Display Page Suite Magazine
    Only you need the ID, the module create the flash file and display
    wherever you want

---
+++ Modules/mod_PagesuiteMiniFlip/mod_PagesuiteMiniFlip.php
@@ -0,0 +1,11 @@
+<?php 
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+include (dirname(__FILE__) . '/Mobile_Detect.php');
+$detect_module = new Mobile_Detect_1;
+// Include the syndicate functions only once
+require_once( dirname(__FILE__).'/helper.php' );
+//echo('Height '.$Height.' Width '.$Width.' ID '.$PageID);
+$code = PagesuiteMiniFlip::getCode( $params,$detect_module );
+require( JModuleHelper::getLayoutPath( 'mod_PagesuiteMiniFlip' ) );
+?>
