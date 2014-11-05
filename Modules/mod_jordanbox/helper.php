1d6e1a8b3f542cbcc7daf1bb8223e47c82fc737c
cuenka <cuenka@gmail.com>
2014-11-05 16:08:07 +0000

    New module for Jordan Website

---
+++ Modules/mod_jordanbox/helper.php
@@ -0,0 +1,17 @@
+<?php
+class jordanbox
+{
+   /**
+     * Retrieves the hello message
+     *
+     * @param array $params An object containing the module parameters
+     * @access public
+     */    
+    public static function getcode( )
+    {
+  	
+  	return 0; 
+  
+    }
+}
+?>
