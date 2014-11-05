0d8eb032880555548207d698221fcb0947820be0
cuenka <cuenka@gmail.com>
2014-11-05 10:10:30 +0000

    Rebuilt this module form  scratch.
    Everything was wrong.

--- Modules/mod_safeContactUsBox/mod_safeContactUsBox.php
+++ Modules/mod_safeContactUsBox/mod_safeContactUsBox.php
@@ -1,11 +1,10 @@
 <?php 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
-include (dirname(__FILE__) . '/Mobile_Detect.php');
-$detect_module = new Mobile_Detect_1;
-// Include the syndicate functions only once
 require_once( dirname(__FILE__).'/helper.php' );
 //echo('Height '.$Height.' Width '.$Width.' ID '.$PageID);
-$code = PagesuiteMiniFlip::getCode( $params,$detect_module );
-require( JModuleHelper::getLayoutPath( 'mod_PagesuiteMiniFlip' ) );
+$Contacts = safeContactUsBox::getCode( $params );
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require( JModuleHelper::getLayoutPath( 'mod_safeContactUsBox' ) );
 ?>
--- Modules/mod_safeContactUsBox/tmpl/default.php
+++ Modules/mod_safeContactUsBox/tmpl/default.php
@@ -1,3 +1,12 @@
 <?php // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
-<?php echo $code; ?>
+<?php foreach ($Contacts as $Contact): ?>
+	<?php if (($Contact->name!=NULL)): ?>
+		<div id="" style="border-bottom: 1px solid #feb715;">
+				<h4><?php echo $Contact->position; ?></h4>
+				<h4><?php echo $Contact->name; ?></h4>
+				<p><?php echo JHtml::_('email.cloak', $Contact->email); ?><br />
+				<?php echo $Contact->phone; ?></p>
+		</div>
+	<?php endif; ?>	
+<?php endforeach; ?>	
