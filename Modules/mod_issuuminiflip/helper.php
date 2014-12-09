548fe5653820d7c9fdce17912201d47b40a2cde0
cuenka <cuenka@gmail.com>
2014-12-09 12:01:17 +0000

    Now I can get the Height without checking manually

--- Modules/mod_issuuminiflip/helper.php
+++ Modules/mod_issuuminiflip/helper.php
@@ -23,14 +23,15 @@ class Issuuminiflip
 	
 		$json = file_get_contents($url);
 		$data = json_decode($json,true);
+		$getheight= Issuuminiflip::get_string_between($data["html"],'height: ','px');
 		if ($params->get('responsive')) 
 		{
 			$data["html"] = str_replace ( '525px' , "100%" , $data["html"] );
-			$data["html"] = str_replace ( '370px' , "auto" , $data["html"] );	
+			$data["html"] = str_replace ( $getheight.'px' , "auto" , $data["html"] );	
 		}else {
-		
+			
 			$data["html"] = str_replace ( '525' , $width , $data["html"] );
-			  $data["html"] = str_replace ( '365' , $height , $data["html"] );	
+			  $data["html"] = str_replace ( $getheight , $height , $data["html"] );	
 		}
 
 	   	$data["html"] = str_replace ( "text-align:left;" , "display:none;" , $data["html"] );
@@ -39,6 +40,15 @@ class Issuuminiflip
 
    	}
 
+    }
+    
+    function get_string_between($string, $start, $end){
+        $string = " ".$string;
+        $ini = strpos($string,$start);
+        if ($ini == 0) return "";
+        $ini += strlen($start);
+        $len = strpos($string,$end,$ini) - $ini;
+        return substr($string,$ini,$len);
     }
 }
 ?>
