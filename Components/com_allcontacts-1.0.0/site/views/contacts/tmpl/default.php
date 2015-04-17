727400124876a296f573462ff80d8c6333aebd77
cuenka <cuenka@gmail.com>
2015-04-17 16:40:54 +0100

    New view, better and cleaner

--- Components/com_allcontacts-1.0.0/site/views/contacts/tmpl/default.php
+++ Components/com_allcontacts-1.0.0/site/views/contacts/tmpl/default.php
@@ -8,173 +8,37 @@
  */
 // no direct access
 defined('_JEXEC') or die;
+?><h1 class="itemTitle">Contact us</h1>
+<?php $department = ""; ?>
 
-JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
-JHtml::_('bootstrap.tooltip');
-JHtml::_('behavior.multiselect');
-JHtml::_('formbehavior.chosen', 'select');
+<?php foreach ($this->items as $i => $item) : ?>
+	<?php if ($item->departament!=$department) : ?>
+     <h2 class="departament" ><?php echo $item->departament; ?></h2>
+    <?php $department=$item->departament; ?>
+    <?php endif; ?> 
+     <?php if ($item->photo):  ?>
+	     <div style="width:29%;margin-right:1%;float: left;">
+	     	<img src="/<?php echo $item->photo; ?>" alt="" />
+	     </div>
+	     <div style="width:69%;margin-left:1%;float: left; color: #3d150e;">
+     <?php else : ?>
+     	<div style="width:100%;color: #3d150e;">
+     <?php endif; ?>
 
-$user = JFactory::getUser();
-$userId = $user->get('id');
-$listOrder = $this->state->get('list.ordering');
-$listDirn = $this->state->get('list.direction');
-$canCreate = $user->authorise('core.create', 'com_allcontacts');
-$canEdit = $user->authorise('core.edit', 'com_allcontacts');
-$canCheckin = $user->authorise('core.manage', 'com_allcontacts');
-$canChange = $user->authorise('core.edit.state', 'com_allcontacts');
-$canDelete = $user->authorise('core.delete', 'com_allcontacts');
-?>
-
-<form action="<?php echo JRoute::_('index.php?option=com_allcontacts&view=contacts'); ?>" method="post" name="adminForm" id="adminForm">
-
-    <?php echo JLayoutHelper::render('default_filter', array('view' => $this), dirname(__FILE__)); ?>
-    <table class="table table-striped" id = "contactList" >
-        <thead >
-            <tr >
-                <?php if (isset($this->items[0]->state)): ?>
-        <th width="1%" class="nowrap center">
-            <?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
-        </th>
-    <?php endif; ?>
-
-    				<th class='left'>
-				<?php echo JHtml::_('grid.sort',  'COM_ALLCONTACTS_CONTACTS_CREATED_BY', 'a.created_by', $listDirn, $listOrder); ?>
-				</th>
-				<th class='left'>
-				<?php echo JHtml::_('grid.sort',  'COM_ALLCONTACTS_CONTACTS_DEPARTAMENT', 'a.departament', $listDirn, $listOrder); ?>
-				</th>
-				<th class='left'>
-				<?php echo JHtml::_('grid.sort',  'COM_ALLCONTACTS_CONTACTS_NAME', 'a.name', $listDirn, $listOrder); ?>
-				</th>
-				<th class='left'>
-				<?php echo JHtml::_('grid.sort',  'COM_ALLCONTACTS_CONTACTS_JOBTITLE', 'a.jobtitle', $listDirn, $listOrder); ?>
-				</th>
-				<th class='left'>
-				<?php echo JHtml::_('grid.sort',  'COM_ALLCONTACTS_CONTACTS_PHONE', 'a.phone', $listDirn, $listOrder); ?>
-				</th>
-				<th class='left'>
-				<?php echo JHtml::_('grid.sort',  'COM_ALLCONTACTS_CONTACTS_EMAIL', 'a.email', $listDirn, $listOrder); ?>
-				</th>
-				<th class='left'>
-				<?php echo JHtml::_('grid.sort',  'COM_ALLCONTACTS_CONTACTS_PHOTO', 'a.photo', $listDirn, $listOrder); ?>
-				</th>
-
-
-    <?php if (isset($this->items[0]->id)): ?>
-        <th width="1%" class="nowrap center hidden-phone">
-            <?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
-        </th>
-    <?php endif; ?>
-
-    				<?php if ($canEdit || $canDelete): ?>
-					<th class="center">
-				<?php echo JText::_('COM_ALLCONTACTS_CONTACTS_ACTIONS'); ?>
-				</th>
-				<?php endif; ?>
-
-    </tr>
-    </thead>
-    <tfoot>
-    <tr>
-        <td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
-            <?php echo $this->pagination->getListFooter(); ?>
-        </td>
-    </tr>
-    </tfoot>
-    <tbody>
-    <?php foreach ($this->items as $i => $item) : ?>
-        <?php $canEdit = $user->authorise('core.edit', 'com_allcontacts'); ?>
-
-        
-        <tr class="row<?php echo $i % 2; ?>">
-
-            <?php if (isset($this->items[0]->state)): ?>
-                <?php $class = ($canEdit || $canChange) ? 'active' : 'disabled'; ?>
-                <td class="center">
-                    <a class="btn btn-micro <?php echo $class; ?>"
-                       href="<?php echo ($canEdit || $canChange) ? JRoute::_('index.php?option=com_allcontacts&task=contactform.publish&id=' . $item->id . '&state=' . (($item->state + 1) % 2), false, 2) : '#'; ?>">
-                        <?php if ($item->state == 1): ?>
-                            <i class="icon-publish"></i>
-                        <?php else: ?>
-                            <i class="icon-unpublish"></i>
-                        <?php endif; ?>
-                    </a>
-                </td>
-            <?php endif; ?>
-
-            				<td>
-
-							<?php echo JFactory::getUser($item->created_by)->name; ?>				</td>
-				<td>
-
-					<?php echo $item->departament; ?>
-				</td>
-				<td>
-				<?php if (isset($item->checked_out) && $item->checked_out) : ?>
-					<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'contacts.', $canCheckin); ?>
-				<?php endif; ?>
-				<a href="<?php echo JRoute::_('index.php?option=com_allcontacts&view=contact&id='.(int) $item->id); ?>">
-				<?php echo $this->escape($item->name); ?></a>
-				</td>
-				<td>
-
-					<?php echo $item->jobtitle; ?>
-				</td>
-				<td>
-
-					<?php echo $item->phone; ?>
-				</td>
-				<td>
-
-					<?php echo $item->email; ?>
-				</td>
-				<td>
-
-					<?php echo $item->photo; ?>
-				</td>
-
-
-            <?php if (isset($this->items[0]->id)): ?>
-                <td class="center hidden-phone">
-                    <?php echo (int)$item->id; ?>
-                </td>
-            <?php endif; ?>
-
-            				<?php if ($canEdit || $canDelete): ?>
-					<td class="center">
-					</td>
-				<?php endif; ?>
-
-        </tr>
+     	<h3><i class="fa fa-user"></i> : <?php echo $this->escape($item->name); ?>
+     	<?php if ($item->jobtitle!="") : ?>
+	    <br />
+	     	<span style="margin-left: 28px;"><?php echo $item->jobtitle; ?></span>
+     	<?php endif; ?>
+     	</h3>
+     	
+     	<?php if ($item->phone!="") : ?>
+     		<p><i class="fa fa-phone"></i> : <?php echo $item->phone; ?></p>
+     	<?php endif; ?>
+     	<?php if ($item->email!="") : ?>
+     		<p class="mailstyle"><i class="fa fa-envelope-o"></i> : <?php echo JHtml::_('email.cloak', $item->email); ?></p>
+     	<?php endif; ?>
+     </div>
+      <div class="clr"></div>
+      <hr class="long"/>
     <?php endforeach; ?>
-    </tbody>
-    </table>
-
-    <?php if ($canCreate): ?>
-        <a href="<?php echo JRoute::_('index.php?option=com_allcontacts&task=contactform.edit&id=0', false, 2); ?>"
-           class="btn btn-success btn-small"><i
-                class="icon-plus"></i> <?php echo JText::_('COM_ALLCONTACTS_ADD_ITEM'); ?></a>
-    <?php endif; ?>
-
-    <input type="hidden" name="task" value=""/>
-    <input type="hidden" name="boxchecked" value="0"/>
-    <input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
-    <input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
-    <?php echo JHtml::_('form.token'); ?>
-</form>
-
-<script type="text/javascript">
-
-    jQuery(document).ready(function () {
-        jQuery('.delete-button').click(deleteItem);
-    });
-
-    function deleteItem() {
-        var item_id = jQuery(this).attr('data-item-id');
-        if (confirm("<?php echo JText::_('COM_ALLCONTACTS_DELETE_MESSAGE'); ?>")) {
-            window.location.href = '<?php echo JRoute::_('index.php?option=com_allcontacts&task=contactform.remove&id=', false, 2) ?>' + item_id;
-        }
-    }
-</script>
-
-
